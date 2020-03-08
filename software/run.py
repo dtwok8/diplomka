ca131c618f185e86394c412b36981eedc1bd891b
dtwok8 <dtwo.k8@gmail.com>
2020-03-08 14:32:28 +0100

    nejakej dodelavky textu

---
+++ software/run.py
@@ -0,0 +1,11 @@
+import os
+
+# pair end
+# 251 dlouhy ready
+
+# vedle se mužeš vygenerovat genom z artu a pak to nahaziš do bowtie a rovnou mu podsuneš referenční gen to znamená nějakej gen z KIR
+# musim vzít referenční genomi a na nich zkusit porovnat jestli se to náhodou nějakýmu nehodí
+
+# určení fitnes funkce
+# dá se tam zakomponovat že v některcýh případech nějaké genomi spolu mají větší pravděpodobnost než jiné
+# specializuješ se jen na KIR geny
