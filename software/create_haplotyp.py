f4ea5a364eb682e7b3858aee864b27f3c8b677e7
dtwok8 <dtwo.k8@gmail.com>
2020-03-20 00:02:57 +0100

    pridani obrazku nk cell s receptory

---
+++ software/create_haplotyp.py
@@ -0,0 +1,26 @@
+
+haplotyp= 3DL3
+
+# tak teoreticky neexistuje žádná alela která by byla jen 000000 ne?
+# Tak by se mohlo udělat, že pokud je to nenulový tak že tam něco je, a když tak tam pak dát konkrétní alelu 
+# akorát já pak teda zjišťuju jakej to je gen, ale jaká je to konkrétní alela mě nezajímá? To je blbost ne? 
+# minimálně na tom HLA tak tam porovnávám alely
+# protože já pak v bowtie podhazuju všechny alely konrkténího genu totiž ..  
+3DL3: 00402, 00802
+2DS2: 00101
+2DL2: 00301
+2DL3: 001
+2DL5B:
+2DS3:
+2DP1: 00201
+2DL1: 00302
+3DP1: 007, 00901
+2DL4: 00102, 00501
+3DL1: 01502
+3DS1: 01301
+2DL5A: 001
+2DS3:
+2DS5: 00201
+2DS1: 00201
+2DS4: 001
+3DL2: 0020105, 0070102
